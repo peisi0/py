0aea79aab0e34a8b52f9d37d96b8578fd07c375e
peisi0 <vikayang777@gmail.com>
2025-02-28 16:55:17 +0800

    Update Mate Task 1.1_360_1.0.py

--- Mate Task 1.1_360_1.0.py
+++ Mate Task 1.1_360_1.0.py
@@ -17,6 +17,10 @@ if status == cv2.Stitcher_OK:
     cv2.imwrite("panorama.jpg", panorama)
     print("create sucessfully！")
     a = cv2.imread("panorama.jpg")
+    cv2.namedWindow("show", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("show", 800, 600)
     cv2.imshow("show", a)
+    if cv2.waitKey(0) == 27:
+        cv2.destroyAllWindows();
 else:
     print("Failed Code：", status)
