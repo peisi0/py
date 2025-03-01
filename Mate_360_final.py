9fbc01d2ca2f658a7031852eba3d28f836ea381f
peisi0 <vikayang777@gmail.com>
2025-03-01 20:03:44 +0800

    Update Mate_360_final.py

--- Mate_360_final.py
+++ Mate_360_final.py
@@ -10,14 +10,14 @@ from PIL import Image, ImageTk
 #        self.canvas = tk.Canvas(master, width=800, height=400)
 #        self.canvas.pack()
 #
-#        # 加载全景图像
+#        
 #        self.image = Image.open(image_path)
 #        self.tk_image = ImageTk.PhotoImage(self.image)
 #
-#        # 创建图像对象
+#        
 #        self.image_obj = self.canvas.create_image(0, 0, anchor='nw', image=self.tk_image)
 #
-#        # 绑定事件
+#        
 #        self.canvas.bind('<ButtonPress-1>', self.on_press)
 #        self.canvas.bind('<B1-Motion>', self.on_drag)
 #
@@ -41,13 +41,13 @@ stitcher = cv2.Stitcher_create()
 
 cap = cv2.VideoCapture(1)
 
-# 检查摄像头是否成功打开
+
 if not cap.isOpened():
     print("无法打开摄像头")
     exit()
 i = 0
 while True:
-    # 读取摄像头帧
+    
     ret, frame = cap.read()
     if not ret:
         print("无法读取帧")
